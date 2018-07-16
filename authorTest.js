241061d4461e57c6361e1a8832d69eb76bfff902
as007 <as008@qq.com>
2018-07-17 07:49:20 +0800

    I don't kown about git

--- authorTest.js
+++ authorTest.js
@@ -1,16 +1,14 @@
-const express = require('express';)
+const express = require('express');
 const router = express.Router();
 const app = express();
 
 //  predicate the router with a check and bail out when needed
 router.use((req, res, next) => {
-    if(!req.headers['x-auth']) return next('router');
+    if (!req.headers['x-auth']) return next('router');
     next();
 });
 
-router.get('/', (req, res) {
-    res.send('hello, user!');
-});
+router.get('/', (req, res) => res.send('hello, user!'));
 
 //  use the router and 401 anything falling through
 app.use('/admin', router, (req, res) => res.sendStatus(401));
